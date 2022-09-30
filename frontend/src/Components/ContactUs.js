33f2de266eb5904d1665861f6fd8065db4fb02eb
Ayanprogrammer11 <ayanliaqat48@gmail.com>
2022-09-30 20:05:50 +0500

    Procile Added

--- frontend/src/Components/ContactUs.js
+++ frontend/src/Components/ContactUs.js
@@ -14,7 +14,7 @@ const handleSubmit = async (e) => {
     const message = document.getElementById("message").value;
 
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/v1/contact/addcontact", {
+    const response = await fetch("https://inotebookbackends.herokuapp.com/api/v1/contact/addcontact", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
