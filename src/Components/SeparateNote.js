8b665614dab64a98ed224bfc5dfb5994ddc846ac
Ayan Liaqat <ayanliaqat48@gmail.com>
2023-03-04 17:41:18 +0600

    removed unnecessary imports

--- src/Components/SeparateNote.js
+++ src/Components/SeparateNote.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, Suspense, lazy } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useParams, useNavigate } from 'react-router-dom'
 
